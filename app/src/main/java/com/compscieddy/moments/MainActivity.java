792bfbd3b44c6d1ade13ea15f4b28eb6c266730a
Eddie Lee <eddieberklee@gmail.com>
2016-06-13 13:47:41 -0700

    tablayout, viewpager placeholder

--- app/src/main/java/com/compscieddy/moments/MainActivity.java
+++ app/src/main/java/com/compscieddy/moments/MainActivity.java
@@ -1,14 +1,22 @@
 package com.compscieddy.moments;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+  @BindView(R.id.tab_layout) TabLayout mTabLayout;
+  @BindView(R.id.view_pager) ViewPager mViewPager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
   }
 }
